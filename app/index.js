e8f2cd96e3a21d3a98e732e3f705dc11b405d6b0
Peter.S <nsaspetersong@gmail.com>
2023-12-23 18:39:06 -0500

    brought in folder

--- app/index.js
+++ app/index.js
@@ -1,12 +1,21 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../componets";
 
 const Home = () => {
+  const router = useRouter();
   return (
     <View>
       <Text>Home</Text>
     </View>
   );
 };
-
 
 export default Home;
